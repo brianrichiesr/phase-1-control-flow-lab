9b473fc39e94889d4e6740437d2f6e78ab40918f
Brian Richie Sr <brianrichiesr@gmail.com>
2023-09-11 22:12:41 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLengthInt){
+  if (rideLengthInt <= 400) {
+    return 'This one is on me!';
+  } else if (rideLengthInt > 400 && rideLengthInt <= 2000) {
+    return 'That will be twenty bucks.'
+  } else if (rideLengthInt > 2000 && rideLengthInt <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipString){
+  switch (tipString) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default:
+      return "Bye."
+  }
 }
